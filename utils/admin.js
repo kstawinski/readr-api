0f8fc677825c3ce9cfc5236d7cec93d08d4d3fab
Kacper <stawinskidev@gmail.com>
2023-02-27 23:25:54 +0100

    create  function to solve Render errors

--- utils/admin.js
+++ utils/admin.js
@@ -1,18 +1,20 @@
 var admin = require('firebase-admin');
 require('dotenv').config();
 
+const envParse = (variable) => variable ? variable.replace(/\\n/g, '\n') : undefined;
+
 admin.initializeApp({
   credential: admin.credential.cert({
     type: 'service_account',
     projectId: 'readr-ea3d2',
-    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    clientId: process.env.FIREBASE_CLIENT_ID,
+    privateKeyId: envParse(process.env.FIREBASE_PRIVATE_KEY_ID),
+    privateKey: envParse(process.env.FIREBASE_PRIVATE_KEY),
+    clientEmail: envParse(process.env.FIREBASE_CLIENT_EMAIL),
+    clientId: envParse(process.env.FIREBASE_CLIENT_ID),
     authUri: 'https://accounts.google.com/o/oauth2/auth',
     tokenUri: 'https://oauth2.googleapis.com/token',
     authProviderX509CertUrl: 'https://www.googleapis.com/oauth2/v1/certs',
-    clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL
+    clientX509CertUrl: envParse(process.env.FIREBASE_CLIENT_X509_CERT_URL)
   }),
 });
 
