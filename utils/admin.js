4b9e38a78bb93f2df4475307ef81f3ad3b0dc481
Kacper <stawinskidev@gmail.com>
2023-02-27 21:59:38 +0100

    fix firebase hidden key import

--- utils/admin.js
+++ utils/admin.js
@@ -1,6 +1,6 @@
 var admin = require('firebase-admin');
 
-var serviceAccount = process.env.NODE_ENV == 'production' ? require('/etc/secrets/firebase-hidden-key.json') : require('./firebase-key.json');
+var serviceAccount = process.env.NODE_ENV == 'production' ? JSON.parse(require('/etc/secrets/firebase-hidden-key.json')) : require('./firebase-key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
