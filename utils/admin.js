cbfa9c9ebd7e799dc47bfdb8832a28752c381bc2
Kacper <stawinskidev@gmail.com>
2023-02-27 23:05:25 +0100

    setup env credentials

--- utils/admin.js
+++ utils/admin.js
@@ -1,9 +1,19 @@
 var admin = require('firebase-admin');
-
-var serviceAccount = process.env.NODE_ENV == 'production' ? require('/etc/secrets/firebase-hidden-key.json') : require('./firebase-key.json');
+require('dotenv').config();
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    type: 'service_account',
+    projectId: 'readr-ea3d2',
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: 'https://accounts.google.com/o/oauth2/auth',
+    tokenUri: 'https://oauth2.googleapis.com/token',
+    authProviderX509CertUrl: 'https://www.googleapis.com/oauth2/v1/certs',
+    clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL
+  }),
 });
 
 const db = admin.firestore();
